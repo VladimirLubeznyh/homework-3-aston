09bf668fbbada67fe8154dbecb1757fcaac9dbc7
VladimirLubeznyh <Vovalyubeznyh@ya.ru>
2023-07-03 14:49:21 +0600

    #2 The first part to check

--- app/src/main/java/ru/lyubeznyh/recyclerview/MainActivity.kt
+++ app/src/main/java/ru/lyubeznyh/recyclerview/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.lyubeznyh.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var countries: RecyclerView
+    private val adapter: CountriesListAdapter by lazy {
+        CountriesListAdapter(randomCountries(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countries = findViewById(R.id.rvCountryList)
+        countries.adapter = adapter
     }
 }
